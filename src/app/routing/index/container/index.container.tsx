59acdb07f751d87f8a122b43db3569723e055cd1
jkiskis <jkiskis@exadel.com>
2020-02-14 22:46:28 +0200

    read me

--- src/app/routing/index/container/index.container.tsx
+++ src/app/routing/index/container/index.container.tsx
@@ -6,7 +6,9 @@ class IndexPage extends React.Component {
 			<div className={'hwk-grid-container hwk-grid-container--horizontal-spaces-primary'}>
 				<div className={'hwk-grid-row'}>
 					<div className={'hwk-grid-col hwk-grid-col--12'}>
-						Readme text here
+						<p>
+							Each company has it's own navigation item, as mentioned in each email
+						</p>
 					</div>
 				</div>
 			</div>
